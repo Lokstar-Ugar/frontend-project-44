f19cf787db346e06b6e73c4046785a72f657ee1e
Денис Пиявкин <engin.pd.m@gmail.com>
2024-01-25 17:35:49 +1000

    add src/Games/even.js

--- src/Games/even.js
+++ src/Games/even.js
@@ -2,7 +2,7 @@ import getRandomIt from '../utils.js';
 import greeting from '../index.js';
 
 const rule = 'Answer "yes" if the number is even, otherwise answer "no".';
-const isEven = (number) => number % 2 === 0 ? 'yes' : 'no';
+const isEven = (number) => number % 2 === 0;
 
 const evenGame = () => {
    const randomNum = getRandomIt(0, 50);
