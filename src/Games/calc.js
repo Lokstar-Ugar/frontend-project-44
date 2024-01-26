ada5a10e9a0df8f6c24a2429de0b26cd0cf68c99
Денис Пиявкин <engin.pd.m@gmail.com>
2024-01-26 23:41:13 +1000

    add  src/Games/calc.js

--- src/Games/calc.js
+++ src/Games/calc.js
@@ -1,15 +1,9 @@
 import getRandomIt from '../utils.js';
 import greeting from '../index.js';
-// import { randomCalculation } from './index.js';
 
 const rule = 'What is the result of the expression?';
 
 const calculate = (num1, num2, operation) => {
-    // const operations = ['*', '-', '+'];
-    // const randomIndex = Math.floor(Math.random() * operations.length);
-    // const operation = operations[randomIndex];
-    // const num1 = getRandomIt(1, 10);
-    // const num2 = getRandomIt(1, 10);
     switch (operation) {
         case '*':
           return num1 * num2;
