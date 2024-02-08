bdc8ea30fe22e7505489b3fef0695f42d46db8b6
Денис Пиявкин <engin.pd.m@gmail.com>
2024-02-08 15:54:06 +1000

    add src/Games/

---
+++ src/Games/progression.js
@@ -0,0 +1,31 @@
+import getRandomIt from '../utils.js';
+import greeting from '../index.js';
+
+const rule = 'What number is missing in the progression?';
+
+const arithmeticProgression = (step) => {
+  const result = [];
+  let current = getRandomIt(5, 5);
+  for (let i = 0; i < getRandomIt(5, 10); i += 1) {
+    result.push(current);
+    current += step;
+  }
+  return result;
+};
+
+const progressionGame = () => {
+  const step = getRandomIt(1, 5);
+  const progression = arithmeticProgression(step);
+  const randomIndex = getRandomIt(0, progression.length - 1);
+  const correctAnswer = progression[randomIndex].toString();
+  const hiddenIndex = '..';
+  progression[randomIndex] = hiddenIndex;
+  const question = progression.join(' ');
+  return [question, correctAnswer];
+};
+
+const startProgression = () => {
+  greeting(progressionGame, rule);
+};
+
+export default startProgression;
